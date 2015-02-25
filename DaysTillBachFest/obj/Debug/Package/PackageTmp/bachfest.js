6d560a82a9c9f8f72b4a967ff25138f14fc6677d
asven <asvennes@gmail.com>
2015-02-25 16:48:16 -0600

    initial

---
+++ DaysTillBachFest/obj/Debug/Package/PackageTmp/bachfest.js
@@ -0,0 +1,28 @@
+﻿
+function daysUntil()
+{
+    setInterval(function ()
+    {
+        var bachDay = new Date("7/16/2015");
+        var difference = (bachDay - new Date());
+        var days = Math.floor( difference / ((60 * 60 * 1000 * 24) * 1));
+        var hours = Math.floor((difference % (60 * 60 * 1000 * 24)) / (60 * 60 * 1000) * 1);
+        var minutes = Math.floor(((difference % (60 * 60 * 1000 * 24)) % (60 * 60 * 1000)) / (60 * 1000) * 1);
+        var seconds = Math.floor((((difference % (60 * 60 * 1000 * 24)) % (60 * 60 * 1000)) % (60 * 1000)) / 1000 * 1);
+        var millseconds = Math.floor((((difference % (60 * 60 * 1000 * 24)) % (60 * 60 * 1000)) % (60 * 1000)) % 1000 * 1);
+        $('#dateDiv').html(days + ":" + hours + ":" + minutes + ":" + seconds + ":" + millseconds);
+    }, 10);
+
+    $("#dateDiv").css("font-size", $(window).width() / 7);
+    $("#dateDiv").css("padding-top", $(window).height() / 4);
+    $("#dateDiv").css("padding-left", $(window).width() / 15);
+
+    $(window).resize(resizeStuff);
+}
+
+function resizeStuff()
+{
+    $("#dateDiv").css("font-size", $(window).width() / 7);
+    $("#dateDiv").css("padding-top", $(window).height() / 4);
+    $("#dateDiv").css("padding-left", $(window).width() / 15);
+}
