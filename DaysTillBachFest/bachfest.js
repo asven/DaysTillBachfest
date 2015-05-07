bd5eb781a91d22e49ce83799993383e2a940cb54
asven <asvennes@gmail.com>
2015-05-07 13:58:15 -0500

    it's ready for the most part

--- DaysTillBachFest/bachfest.js
+++ DaysTillBachFest/bachfest.js
@@ -10,7 +10,7 @@ function daysUntil()
         var minutes = Math.floor(((difference % (60 * 60 * 1000 * 24)) % (60 * 60 * 1000)) / (60 * 1000) * 1);
         var seconds = Math.floor((((difference % (60 * 60 * 1000 * 24)) % (60 * 60 * 1000)) % (60 * 1000)) / 1000 * 1);
         var millseconds = Math.floor((((difference % (60 * 60 * 1000 * 24)) % (60 * 60 * 1000)) % (60 * 1000)) % 1000 * 1);
-        if (days <= 0 && hours <= 0 && minutes <= 0 && seconds <= 0 && millseconds <= 0)
+        if ((days <= 0 && hours <= 0 && minutes <= 0 && seconds <= 0 && millseconds <= 0) || window.location.href.indexOf("?test") > -1)
         {
             $("#world").show();
             $("#dateDiv").hide();
