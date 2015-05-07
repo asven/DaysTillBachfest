302d1856e4cd6dcd823fce9cfc6f55d3b8a1fdf3
asven <asvennes@gmail.com>
2015-05-07 14:07:24 -0500

    some stuff that probbaly don't need to be committed.

--- DaysTillBachFest/obj/Debug/Package/PackageTmp/bachfest.js
+++ DaysTillBachFest/obj/Debug/Package/PackageTmp/bachfest.js
@@ -1,7 +1,7 @@
 ﻿
 function daysUntil()
 {
-    setInterval(function ()
+    var intervalID = setInterval(function ()
     {
         var bachDay = new Date("7/16/2015");
         var difference = (bachDay - new Date());
@@ -10,6 +10,12 @@ function daysUntil()
         var minutes = Math.floor(((difference % (60 * 60 * 1000 * 24)) % (60 * 60 * 1000)) / (60 * 1000) * 1);
         var seconds = Math.floor((((difference % (60 * 60 * 1000 * 24)) % (60 * 60 * 1000)) % (60 * 1000)) / 1000 * 1);
         var millseconds = Math.floor((((difference % (60 * 60 * 1000 * 24)) % (60 * 60 * 1000)) % (60 * 1000)) % 1000 * 1);
+        if ((days <= 0 && hours <= 0 && minutes <= 0 && seconds <= 0 && millseconds <= 0) || window.location.href.indexOf("?test") > -1)
+        {
+            $("#world").show();
+            $("#dateDiv").hide();
+            clearInterval(intervalID);
+        }
         $('#dateDiv').html(days + ":" + hours + ":" + minutes + ":" + seconds + ":" + millseconds);
     }, 10);
 
